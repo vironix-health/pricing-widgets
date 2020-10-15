6bbb5224edeeb8b1002108f081d07b6cfd9a0c79
Botros Toro <btoro93@gmail.com>
2020-10-15 09:51:58 -0400

    creation of widget

--- react-javascript/src/App.js
+++ react-javascript/src/App.js
@@ -1,27 +1,115 @@
-import { LiveEditor, LiveError, LivePreview, LiveProvider } from "react-live";
+import {
+  Button,
+  Card,
+  InputNumber,
+  Form,
+  Select,
+  Typography,
+  Space,
+} from "antd";
+import "./App.less";
+import React, { useState, useEffect } from "react";
+import { Row, Col } from "antd";
 
-import React from 'react';
-
-const code = `
-() => {
-    const [count, setCount] = React.useState(0);
-    return (
-        <>
-            <p>Count: {count}</p>
-            <button onClick={() => setCount(count + 1)}>+1</button>
-            <button onClick={() => setCount(count - 1)}>-1</button>
-        </>
-    )
+function numberWithCommas(x) {
+  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 }
-`;
+
+const BloodTestCompare = () => {
+  const [OrgSize, setOrgSize] = useState(10);
+  const [SelectedPlan, setSelectedPlan] = useState("free");
+  const [PriceCovidX, setPriceCovidX] = useState(0);
+  const [PriceTest, setPriceTest] = useState(0);
+  const [CostDiagnosticTest, setCostDiagnosticTest] = useState(100);
+
+  const handleChange = (value) => {
+    setSelectedPlan(value);
+  };
+
+  useEffect(() => {
+    if (SelectedPlan === "free") {
+      if (OrgSize >= 40) {
+        setSelectedPlan("standard");
+      } else {
+        setPriceCovidX(0);
+        setPriceTest(OrgSize * CostDiagnosticTest);
+      }
+    } else if (SelectedPlan === "standard") {
+      setPriceCovidX(5 * OrgSize);
+      setPriceTest(OrgSize * CostDiagnosticTest);
+    }
+  }, [OrgSize, SelectedPlan, CostDiagnosticTest]);
+
+  return (
+    <Form>
+      <Row style={{ border: "1px solid" }}>
+        <Col span={14} style={{ padding: "32px" }}>
+          <Space direction="vertical" size={16}>
+            <Form.Item label="Select Plan">
+              <Select
+                defaultValue="free"
+                style={{ width: 120 }}
+                onChange={handleChange}
+                value={SelectedPlan}
+              >
+                <Select.Option value="free">Free</Select.Option>
+                <Select.Option value="standard">Standard</Select.Option>
+              </Select>
+            </Form.Item>
+            <Form.Item label="Organization Members">
+              <InputNumber
+                value={OrgSize}
+                onChange={(value) => {
+                  setOrgSize(value);
+                }}
+              />
+            </Form.Item>
+            <Form.Item label="Covid19 Diagnostic Test Cost">
+              ${" "}
+              <InputNumber
+                value={CostDiagnosticTest}
+                onChange={(value) => {
+                  setCostDiagnosticTest(value);
+                }}
+              />
+            </Form.Item>
+          </Space>
+        </Col>
+        <Col
+          span={10}
+          style={{
+            backgroundColor: "#993333",
+            padding: "32px",
+          }}
+        >
+          <Row>
+            <Space direction="vertical" size={16}>
+              <Typography.Title level={3} style={{ color: "#fff" }}>
+                It would cost your organization{" "}
+                <span style={{ color: "#eeb55f" }}>
+                  ${numberWithCommas(PriceCovidX * 12)}/year
+                </span>{" "}
+                compared to{" "}
+                <span style={{ color: "#eeb55f" }}>
+                  ${numberWithCommas(PriceTest)}
+                </span>{" "}
+                to test your organization members{" "}
+                <span style={{ textDecoration: "underline" }}>once</span> for
+                Covid-19.
+              </Typography.Title>
+            </Space>
+          </Row>
+        </Col>
+      </Row>
+    </Form>
+  );
+};
 
 function App() {
   return (
-    <LiveProvider code={code}>
-      <LiveEditor />
-      <LiveError />
-      <LivePreview />
-    </LiveProvider>
+    <div className="App" style={{ padding: "50px", width: "800px" }}>
+      <BloodTestCompare />
+    </div>
   );
 }
 
--- react-typescript/src/App.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import Typist from "react-typist";
-
-const App = () => <Typist>React is super wild and you can do so many cool things with it!!</Typist>;
-
-export default App;
